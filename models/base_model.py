a72a1370a3aee44259e3e347055c2fb9bd77e98e
dani <danielshitaye10@gmail.com>
2024-07-06 07:37:53 -0700

    base class definition added

---
+++ models/base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+   This module holds the class definition of Base which all
+   other classes inherit (extend) from
+
+"""
+
+
+class BaseModel():
+    """
+       class BaseModel act as a base class
+       and holds common class level attributes and methods
+       shared among all other classes
+
+    """
