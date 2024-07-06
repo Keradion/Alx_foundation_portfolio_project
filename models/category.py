2720ff3f99c627a1b732384ca4c2ada21f8502b1
dani <danielshitaye10@gmail.com>
2024-07-06 08:28:35 -0700

    classe's

---
+++ models/category.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+   This Module Holds The Definition Of Class Category
+"""
+
+
+class Category(BaseModel):
+    """
+       Category Class Extends BaseModel Class And
+       Holds Attributes and Methods To Work With
+       Category Class Objects
+
+    """
+    category_name = ""
+    cateorgy_level = 0
---
+++ models/expense.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+   Module Holds Class Expense Definition
+"""
+import models
+
+
+class Expense(BaseModel):
+    """
+       Expense Class Holds Attributes and Methods
+       To Work With Expense Class Objects
+
+    """
+    __current_balance = 0
+
+    def calculate_deposit(self):
+        """
+           Method That Compute Deposits In Current
+           Ending Balance
+
+        """
+
+    def calculate_witdrawn(self):
+        """
+           Method That Deducts An Amount From Current
+           Ending Balance
+
+        """
