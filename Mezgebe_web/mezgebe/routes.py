300d58b144f1567e4e981e2acdfdd09823f948c8
Daniel <danielshitaye10@gmail.com>
2024-09-05 21:18:44 +0300

    user authentication completed

--- Mezgebe_web/mezgebe/routes.py
+++ Mezgebe_web/mezgebe/routes.py
@@ -3,7 +3,7 @@ from mezgebe.models import User
 from flask import render_template, flash, redirect, url_for
 from mezgebe.forms import UserRegisterationForm, UserLoginForm
 from mezgebe.models import User, Expense, Category
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user, login_required 
 
 
 
@@ -24,6 +24,8 @@ def about():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """ A Route To Handle a New User Registeration Process """
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     registeration_form = UserRegisterationForm()
     if registeration_form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(registeration_form.password.data).decode('utf-8')
@@ -43,6 +45,8 @@ def register():
 @app.route('/Login', methods=['GET', 'POST'])
 def login():
     """ A Route To Handle a User Login Process """
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     login_form = UserLoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email=login_form.email.data).first()
@@ -52,3 +56,18 @@ def login():
         else:
             flash('Invalid credential. Please check email or password!')
     return render_template('login.html', form=login_form)
+
+
+
+
+@app.route('/Logout')
+def logout():
+    """ A Route TO Handle a User Logout Process """
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    """ A Route To Handle a User Restriction """
+    return render_template('account.html', title='Account')
