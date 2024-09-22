70ec2dc4c9f496512005cec9a3790eafad60b66c
Daniel Berihun <danielshitaye10@gmail.com>
2024-09-22 16:49:47 +0300

    Update config.py
    Email and app password removed (not secure way)

--- Mezgebe_web/mezgebe/config.py
+++ Mezgebe_web/mezgebe/config.py
@@ -8,6 +8,6 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'danielshitaye10@gmail.com'
-    MAIL_PASSWORD ='jqkp kmxe xpam uxfd'
+    MAIL_USERNAME = '@gmail.com'
+    MAIL_PASSWORD ='jqkp'
 
